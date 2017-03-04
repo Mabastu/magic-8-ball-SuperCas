dcafa17534f826dd93e3519465d92daf6bb153a3
SuperCas <christian_siever@web.de>
2017-03-04 16:54:40 +0100

    8 Ball Submission

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Christian Siever on 04.03.17.
+//  Copyright © 2017 Christian Siever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    
+    var randomBallNumber : Int = 0
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        changeBallImage()
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        changeBallImage()
+        
+    }
+
+    
+    func changeBallImage(){
+       
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        changeBallImage()
+    }
+    
+}
+
